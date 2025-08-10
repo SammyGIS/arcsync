2ba81d746bd015c06441c9dc2edb15d2083a72ff
sammygis <adedoyinsamuel25@gmail.com>
2025-08-10 21:41:14 +0100

    chore: create configurations files

---
+++ config/constant.py
@@ -0,0 +1,31 @@
+#  Default Extent for Layers 
+EXTENT = {
+    "xmin": -180,
+    "ymin": -90,
+    "xmax": 180,
+    "ymax": 90,
+    "spatialReference": {
+        "wkid": 4326
+    }
+}
+
+#  Default Fields for a  Feature Layer 
+DEFAULT_LAYER_FIELDS = [
+    {
+        "name": "fullname",
+        "type": "esriFieldTypeString",
+        "alias": "Full Name",
+        "sqlType": "sqlTypeVarchar",
+        "length": 255,
+        "nullable": True,
+        "editable": True
+    }
+]
+
+# Default Tags and Metadata 
+DEFAULT_TAGS = ["", "Feature Service", "Point Layer"]
+DEFAULT_SNIPPET = "Hosted feature service created dynamically via script."
+DEFAULT_DESCRIPTION = "This is an empty feature service to which a point layer will be added."
+
+#  Default Service Capabilities 
+DEFAULT_CAPABILITIES = "Create,Query,Update,Delete,Sync"
